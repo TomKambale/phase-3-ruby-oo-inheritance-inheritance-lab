3703dd6965e7fae03548c0e9144553db4de30143
Thomas <thomaskambale1@gmail.com>
2023-05-23 22:04:30 +0300

    Completed assignment

---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+class User
+    attr_reader : :first_name, :last_name
+    def initialize(first_name, last_name)
+        @first_name = Firstname
+        @last_name = Lastname
+
+    end
+end
+
+def first_name
+    @first_name
+end
+
+def last_name
+    @last_name
+end
+end
